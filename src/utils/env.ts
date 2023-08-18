e0062ee9dff5ac086820742dc895b5ff929f8c81
ふぁ <yuki@yuki0311.com>
2023-08-18 12:04:41 +0900

    add dev env
    Signed-off-by: ふぁ <yuki@yuki0311.com>

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,6 +1,10 @@
 import * as dotenv from 'dotenv';
-dotenv.config();
+
+dotenv.config({
+  path: process.env.DEVELP == 'true' ? '.develop.env' : undefined,
+});
 
 export const getString = (key: string, option?: string): string => process.env[key] ?? option ?? '';
-export const getNumber = (key: string, option?: number): number =>
-  process.env[key] == undefined ? option ?? 0 : Number(process.env[key]);
+export const getNumber = (key: string, option?: number): number => {
+  return process.env[key] == undefined ? option ?? 0 : Number(process.env[key]);
+};
