cc7b0e66b08613cc40f311f0ff73b490f723ca5b
ふぁ <yuki@yuki0311.com>
2025-04-12 19:11:41 +0900

    update local notify
    Signed-off-by: ふぁ <yuki@yuki0311.com>

--- src/client/linePush.ts
+++ src/client/linePush.ts
@@ -1,11 +1,10 @@
 import PushClient, { type PutFileType } from '@/client/base';
 import { Failure, type Result, Success } from '@/utils/result';
 import axios, { type AxiosResponse } from 'axios';
-import FormData from 'form-data';
 import qs from 'qs';
 
 class LinePushClient extends PushClient {
-  url = 'https://notify-api.line.me/api/notify';
+  url = 'http://127.0.0.1:4535/api/notify';
   token: string;
   putFile: PutFileType;
 
@@ -37,34 +36,10 @@ class LinePushClient extends PushClient {
     message?: string,
     user?: string,
   ): Promise<Result<AxiosResponse, Error>> {
-    const isImage = (e: string | null) => {
-      if (!e) return false;
-      if (e.split('.').length == 1) return false;
-      return ['jpeg', 'png', 'jpg'].includes(e.split('.').slice(-1)[0]);
-    };
     const url = await this.putFile(name, image);
     const text = (user || message) && [user && `${user}>>`, message].flatMap((e) => (e ? [e] : [])).join(' ');
 
-    if (isImage(name)) {
-      const params = new FormData();
-      if (image) params.append('imageFile', image, 'test.png');
-      params.append('message', text ?? '');
-
-      return await axios({
-        method: 'post',
-        url: this.url,
-        headers: {
-          Authorization: `Bearer ${this.token}`,
-          'Content-Type': 'multipart/form-data',
-          ...params.getHeaders(),
-        },
-        data: params,
-      })
-        .then((e) => new Success<AxiosResponse>(e))
-        .catch((e) => new Failure<Error>(e as Error));
-    } else {
-      return this.send([text, url].filter((e) => e).join('\n'));
-    }
+    return this.send([text, url].filter((e) => e).join('\n'));
   }
 }
 export default LinePushClient;
